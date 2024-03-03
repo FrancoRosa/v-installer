5a946ee4a77fabc15f52e2431d792a94b70083a5
FrancoRosa <km115.franco@gmail.com>
2024-03-03 16:39:55 -0500

    Fix installation script

--- setup.sh
+++ setup.sh
@@ -43,12 +43,15 @@ install_dependency "git" "sudo apt-get install  -y git"
 install_dependency "node" "sudo apt install nodejs -y" "curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - "
 install_dependency "pm2" "sudo npm i -g pm2"
 sudo pm2 startup
-wget -O ~/settings.json https://raw.githubusercontent.com/francorosa/v-installer/master/settings.json
+wget -O ~/settings.js https://raw.githubusercontent.com/francorosa/v-installer/master/settings.js
 
 # ask wich version to install
 while true; do
-    read -p "Select mode:\n 1: Install I21A00 - 90% Ullage (TLS450)\n 2: Install I20100 - 100% Ullage (TLS350) (1/2)" yn
-    case $yn in
+    echo "Select mode:"
+    echo "1: Install I21A00 - 90% Ullage (TLS450)"
+    echo "2: Install I20100 - 100% Ullage (TLS350)"
+    read -p "Enter your choice (1 or 2): " choise
+    case $choise in
         [1]* ) echo "${grn}... installing I21A00 - 90% Ullage (TLS450)${rst}"; wget -O ~/api.js https://raw.githubusercontent.com/francorosa/v-installer/master/api_u_build.js; break;;
         [2]* ) echo "${red}... installing I20100 - 100% Ullage (TLS350)${rst}"; wget -O ~/api.js https://raw.githubusercontent.com/francorosa/v-installer/master/api_build.js; break;;
         * ) echo "Please answer 1 or 2.";;
@@ -60,7 +63,7 @@ sudo pm2 start ~/api.js --restart-delay 5000 --max-memory-restart 300M --name "d
 sudo pm2 save
 
 rm z
-echo "${grn}... done${rst}"
+echo "${grn}... installation complete!${rst}"
 
-echo "${grn}... Don't forget to edit settings.js to set the right id, times and email receivers${rst}"
-echo "${grn}... After any settings change run 'sudo pm2 restart dips'${rst}"
+echo "${grn}... don't forget to edit ${red}settings.js ${rst}"
+echo "${grn}... after any settings change run ${red}sudo pm2 restart dips${rst}"
