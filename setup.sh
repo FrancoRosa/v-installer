341238d39b446ab2fed5bd0aabb6ec9d92df0389
FrancoRosa <km115.franco@gmail.com>
2024-03-04 01:30:21 -0500

    create installation folder

--- setup.sh
+++ setup.sh
@@ -43,7 +43,8 @@ install_dependency "git" "sudo apt-get install  -y git"
 install_dependency "node" "sudo apt install nodejs -y" "curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - "
 install_dependency "pm2" "sudo npm i -g pm2"
 sudo pm2 startup
-wget -O ~/settings.json https://raw.githubusercontent.com/francorosa/v-installer/master/settings.json
+mkdir ~/dips
+wget -O ~/dips/settings.json https://raw.githubusercontent.com/francorosa/v-installer/master/settings.json
 
 # ask wich version to install
 while true; do
@@ -52,14 +53,14 @@ while true; do
     echo "2: Install I20100 - 100% Ullage (TLS350)"
     read -p "Enter your choice (1 or 2): " choise
     case $choise in
-        [1]* ) echo "${grn}... installing I21A00 - 90% Ullage (TLS450)${rst}"; wget -O ~/api.js https://raw.githubusercontent.com/francorosa/v-installer/master/api_u_build.js; break;;
-        [2]* ) echo "${red}... installing I20100 - 100% Ullage (TLS350)${rst}"; wget -O ~/api.js https://raw.githubusercontent.com/francorosa/v-installer/master/api_build.js; break;;
+        [1]* ) echo "${grn}... installing I21A00 - 90% Ullage (TLS450)${rst}"; wget -O ~/dips/api.js https://raw.githubusercontent.com/francorosa/v-installer/master/api_u_build.js; break;;
+        [2]* ) echo "${red}... installing I20100 - 100% Ullage (TLS350)${rst}"; wget -O ~/dips/api.js https://raw.githubusercontent.com/francorosa/v-installer/master/api_build.js; break;;
         * ) echo "Please answer 1 or 2.";;
     esac
 done
 
 
-sudo pm2 start ~/api.js --restart-delay 5000 --max-memory-restart 300M --name "dips"
+sudo pm2 start ~/dips/api.js --restart-delay 5000 --max-memory-restart 300M --name "dips"
 sudo pm2 save
 
 rm z
