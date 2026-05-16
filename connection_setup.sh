0fc2ab9afad2badf1f7add09c6603a8ea580c07a
FrancoRosa <km115.franco@gmail.com>
2026-05-16 18:52:29 -0500

    Update documentation

--- connection_setup.sh
+++ connection_setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
+# colors
+red='\e[1;31m'
+grn='\e[1;32m' 
+org='\e[1;93m' 
+rst='\e[0m'
+echo "${grn}... Adding connection watchdog ${rst}"
+
 wget -O ~/dips/connection.sh https://raw.githubusercontent.com/francorosa/v-installer/master/connection.sh
 mkdir ~/dips
-pm2 start connection.sh --name "connection" --interpreter bash
+sudo pm2 start connection.sh --name "connection" --interpreter bash
 sudo pm2 save
 
 echo "${grn}... connection installation complete!${rst}"
--- readme.md
+++ readme.md
@@ -1,5 +1,13 @@
 # installer
+
 Copy, paste and run the following line to install dependencies and set PM2 to manage the service
+
 ```bash
 wget -qO z bit.ly/knes && sh z
+```
+
+The folowwing code is to add a watchdog that reboots if no internet connection in a while
+
+```bash
+curl -fsSL bit.ly/kncnx | sh
 ```
